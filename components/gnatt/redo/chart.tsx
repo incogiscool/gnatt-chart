15117266357ec7612fac1ff3f71b73d7b1a28727
incogiscool <94598096+incogiscool@users.noreply.github.com>
2023-08-30 17:23:40 +0400

    gnat chart

--- components/gnatt/redo/chart.tsx
+++ components/gnatt/redo/chart.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { LocalGnattSection } from "../schema";
 import { useGnattData } from "./state";
 
 const GnattChart = () => {
@@ -10,6 +11,7 @@ const GnattChart = () => {
 
   const daysInMonth = (year: number, month: number) =>
     new Date(year, month + 1, 0).getDate();
+
   const getDaysOfMonth = (year: number, month: number) => {
     const totalDays = daysInMonth(year, month);
     const daysArray = [];
@@ -21,23 +23,65 @@ const GnattChart = () => {
     return daysArray;
   };
 
+  const days = getDaysOfMonth(currentYear, currentMonth);
+
   const rows = 1;
-  const columns = 30;
+  const columns = days.length;
+
+  function toggleExpandSection(index: number) {
+    const section = data?.sections[index];
+
+    if (!section) return;
+
+    let updatedSections = data?.sections;
+    updatedSections[index] = {
+      ...section,
+      expanded: !section?.expanded,
+    };
+
+    setData({
+      ...data,
+      sections: updatedSections,
+    });
+  }
 
   return (
     <div className="w-full h-full">
       <div
         style={{
-          gridTemplateColumns: "repeat(30, minmax(0, 1fr))",
+          gridTemplateColumns: `repeat(${columns}, minmax(0, 1fr))`,
         }}
         className={`grid  grid-rows-${rows}`}
       >
         {Array.from({ length: rows * columns }).map((_, index) => (
           <div key={index} className="h-[49px] py-4 text-sm text-center border">
-            {index}
+            {index + 1}
           </div>
         ))}
       </div>
+      {data?.sections.map((section, index) => {
+        return (
+          <div key={section.id} className="w-full">
+            <div
+              className="min-h-[50px] border"
+              onClick={() => toggleExpandSection(index)}
+            >
+              <p>{section.title}</p>
+            </div>
+            <div className="bg-slate-200">
+              {section.expanded && (
+                <div>
+                  {section.tasks.map((task) => {
+                    return (
+                      <div className="h-[50px] w-full border">{task.title}</div>
+                    );
+                  })}
+                </div>
+              )}
+            </div>
+          </div>
+        );
+      })}
 
       {/* <div className="flex w-fit gap-2">
         {daysOfMonth.map((day) => {
